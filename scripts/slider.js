4d2b43f1e8c36e3ef66c3e619254491099fad0b9
marepic <alex.garnov63@gmail.com>
2021-11-19 23:24:39 +0300

    first commit

---
+++ scripts/slider.js
@@ -0,0 +1,14 @@
+const slider = $('.products').bxSlider({
+    pager: false,
+    controls: false
+});
+
+$('.products-slider__arrow--direction--next').click(e => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
+
+$('.products-slider__arrow--direction--prev').click(e => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
