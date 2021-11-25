c12a8ee39208814a6770a8874c707db7f1fb4953
marepic <alex.garnov63@gmail.com>
2021-11-25 15:34:28 +0300

    ops repair landing

--- scripts/ops.js
+++ scripts/ops.js
@@ -1,118 +1,144 @@
-const sections = $("section");
-const display = $(".maincontent");
-const sideMenu = $(".fixed-menu");
-const menuItems = sideMenu.find(".fixed-menu__item");
+(function () {
+    const sections = $("section");
+    const display = $(".maincontent");
+    const sideMenu = $(".fixed-menu");
+    const menuItems = sideMenu.find(".fixed-menu__item");
 
-let inScroll = false;
+    const mobileDetect = new MobileDetect(window.navigator.userAgent);
+    const isMobile = mobileDetect.mobile();
 
-sections.first().addClass("active");
+    let inScroll = false;
 
-const countSectionPosition = (sectionEq) => {
-    const position =  sectionEq * -100;
+    sections.first().addClass("active");
 
-    if (isNaN(position)) {
-        console.error("передано не верное значение в countSectionPosition");
-        return 0;
-    }
+    const countSectionPosition = sectionEq => {
+        const position = sectionEq * -100;
 
-    return position;
-};
+        if (isNaN(position)) {
+            console.error("передано не верное значение в countSectionPosition");
+            return 0;
+        }
 
-const changeMenuThemeFarSection = (sectionEq) => {
-    const currentSection = sections.eq(sectionEq);
-    const menuTheme = currentSection.attr("data-sidemenu-theme");
-    const activeClass = "fixed-menu--shadowed";
+        return position;
+    };
 
-    if (menuTheme == "black") {
-        sideMenu.addClass(activeClass);
-    } else {
-        sideMenu.removeClass("fixed-menu--shadowed");
-    }
-};
+    const changeMenuThemeForSection = (sectionEq) => {
+        const currentSection = sections.eq(sectionEq);
+        const menuTheme = currentSection.attr("data-sidemenu-theme");
+        const activeClass = "fixed-menu--shadowed";
 
-const resetActiveClassForItem = (items, itemEq, activeClass) => {
-    items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
-};
+        if (menuTheme == "black") {
+            sideMenu.addClass(activeClass);
+        } else {
+            sideMenu.removeClass(activeClass);
+        }
+    };
 
-const performTransition = (sectionEq) => {
-    if (inScroll) return;
+    const resetActiveClassForItem = (items, itemEq, activeClass) => {
+        items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
+    };
 
-    const transitionOver = 1000;
-    const mouseInertiaOver = 300;
+    const performTransition = (sectionEq) => {
+        if (inScroll) return;
 
-    inScroll = true;
+        const transitionOver = 1000;
+        const mouseInertiaOver = 300;
 
-    const position = countSectionPosition(sectionEq);
+        inScroll = true;
 
-    changeMenuThemeFarSection(sectionEq);
+        const position = countSectionPosition(sectionEq);
 
-    display.css({
-        transform: `translateY(${position}%)`,
+        changeMenuThemeForSection(sectionEq);
+
+        display.css({
+            transform: `translateY(${position}%)`
+        });
+
+        resetActiveClassForItem(sections, sectionEq, "active");
+
+        setTimeout(() => {
+            inScroll = false;
+            resetActiveClassForItem(menuItems, sectionEq, "fixed-menu__item--active");
+        }, transitionOver + mouseInertiaOver);
+    };
+
+    const viewportScroller = () => {
+        const activeSection = sections.filter(".active");
+        const nextSection = activeSection.next();
+        const prevSection = activeSection.prev();
+
+        return {
+            next() {
+                if (nextSection.length) {
+                    performTransition(nextSection.index());
+                }
+            },
+            prev() {
+                if (prevSection.length) {
+                    performTransition(prevSection.index());
+                }
+            },
+        };
+    };
+
+    $(window).on("wheel", e => {
+        const deltaY = e.originalEvent.deltaY;
+        const scroller = viewportScroller();
+
+        if (deltaY > 0) {
+            scroller.next();
+        }
+
+        if (deltaY < 0) {
+            scroller.prev();
+        }
     });
 
-    resetActiveClassForItem(sections, sectionEq, "active");
-    
-    setTimeout(() => {
-        inScroll = false;
-        resetActiveClassForItem(menuItems, sectionEq, "fixed-menu__item--active");
-    }, transitionOver + mouseInertiaOver);
-};
+    $(window).on("keydown", (e) => {
+        const tagName = e.target.tagName.toLowerCase();
+        const userTypingInInputs = tagName == "input" || tagName == "textarea";
+        const scroller = viewportScroller();
 
-const viewportScroller = () => {
-    const activeSection = sections.filter("active");
-    const nextSection = activeSection.next();
-    const prevSection = activeSection.prev();
+        if (userTypingInInputs) return;
 
-    return {
-        next() {
-            if (nextSection.length) {
-                performTransition(nextSection.index());
-            }
-        },
-        prev() {
-            if (prevSection.length) {
-                performTransition(prevSection.index());
-            }
-        },
-    };
-};
+        switch (e.keyCode) {
+            case 38:
+                scroller.prev();
+                break;
 
-$(window).on("wheel", e => {
-    const deltaY = e.originalEvent.deltaY;
-    const scroller = viewportScroller();
+            case 40:
+                scroller.next();
+                break;
+        }
+    });
 
-    if (deltaY > 0) {
-        scroller.next();
+    $("wrapper").on("touchmove", e => e.preventDefault());
+
+    $("[data-scroll-to]").click(e => {
+        e.preventDefault();
+
+        const $this = $(e.currentTarget);
+        const target = $this.attr("data-scroll-to");
+        const reqSection = $(`[data-section-id=${target}]`);
+
+        performTransition(reqSection.index());
+    });
+
+    if (isMobile) {
+        // https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+        $(function () {
+            $("body").swipe({
+                swipe: function (event, direction) {
+                    const scroller = viewportScroller();
+                    let scrollDirection = "";
+                    if (direction === "up") scrollDirection = "next";
+                    if (direction === "down") scrollDirection = "prev";
+                    if(scrollDirection !== ""){
+                        scroller[scrollDirection]();
+
+                    }
+                },
+            });
+        });
     }
-
-    if (deltaY < 0) {
-        scroller.prev();
-    }
-});
-
-$(window).on("keydown", (e) => {
-    const  tagName = e.target.tagName.toLowerCase();
-    const userTypingInInputs = tagName == "input" || tagName == "textarea";
-    const scroller = viewportScroller();
-
-    if (userTypingInInputs) return; 
-    switch (e.keyCode) {
-        case 38:
-            scroller.prev(); 
-            break;
-
-        case 40:
-            scroller.next();
-            break;
-    }
-});
-
-$("[data-scroll-to]").click(e => {
-    e.preventDefault();
-
-    const $this = $(e.currentTarget);
-    const target = $this.attr("data-scroll-to");
-    const reqSection = $(`[data-section-id=${target}]`);
-
-    performTransition(reqSection.index());
-});
+}());
